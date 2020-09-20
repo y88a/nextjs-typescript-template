eb55490f9557877e99e879c4e62b97645bad4e8d
Yuta <yuta.miura82@gmail.com>
2020-09-21 00:19:30 +0900

    fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,22 @@
+const path = require('path');
+const rootPath = path.resolve(__dirname, '../');
+
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../components/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss'],
+  webpackFinal: async (baseConfig) => {
+    const nextConfig = require('../next.config.js');
+    baseConfig.resolve.alias['@'] = rootPath
+    baseConfig.resolve.alias['~'] = rootPath
+
+    // merge whatever from nextConfig into the webpack config storybook will use
+    return { ...baseConfig };
+  },
+  stories: [
+    '../stories/**/*.stories.mdx', 
+    '../components/**/*.stories.@(ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links', 
+    '@storybook/addon-essentials', 
+    '@storybook/preset-scss'
+  ],
 }
